7bfb054bbf2b2866795d4212ec587c9f3bb7f39e
Washington Antunes <washington.cead@gmail.com>
2023-07-18 21:54:20 -0300

    criando projeto

---
+++ src/main/java/br/com/wti/EspecialistaAngularSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.wti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspecialistaAngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspecialistaAngularSpringApplication.class, args);
+	}
+
+}
